f1fb831f247b5034adb56a74001db66b8acb5ab4
kOstovic <ostacro.jug2@gmail.com>
2018-05-02 13:25:01 +0200

    try fix for winston2

---
+++ test/logic.js
@@ -0,0 +1,254 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+'use strict';
+/**
+ * Write the unit tests for your transction processor functions here
+ */
+/*
+const AdminConnection = require('composer-admin').AdminConnection;
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
+const path = require('path');
+
+const chai = require('chai');
+chai.should();
+chai.use(require('chai-as-promised'));
+
+const namespace = 'pii.szg';
+const assetType = 'UniversityComponent';
+const assetNS = namespace + '.' + assetType;
+const participantType = 'Member';
+const participantType2 = 'SystemAdministrator';
+const participantNS = namespace + '.' + participantType;
+const participantNS2 = namespace + '.' + participantType2;
+
+describe('#' + namespace, () => {
+    // In-memory card store for testing so cards are not persisted to the file system
+    const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
+
+    // Embedded connection used for local testing
+    const connectionProfile = {
+        name: 'embedded',
+        'x-type': 'embedded'
+    };
+
+    // Name of the business network card containing the administrative identity for the business network
+    const adminCardName = 'admin';
+
+    // Admin connection to the blockchain, used to deploy the business network
+    let adminConnection;
+
+    // This is the business network connection the tests will use.
+    let businessNetworkConnection;
+
+    // This is the factory for creating instances of types.
+    let factory;
+
+    // These are the identities for Alice and Bob.
+    const ferovacCardName = '0036444555';
+    const ferovacProfesorCardName = '0036555555';
+    const filozofCardName = '1111223322';
+
+    // These are a list of receieved events.
+    let events;
+
+    let businessNetworkName;
+
+    before(async () => {
+        // Generate certificates for use with the embedded connection
+        const credentials = CertificateUtil.generate({ commonName: 'admin' });
+
+        // Identity used with the admin connection to deploy business networks
+        const deployerMetadata = {
+            version: 1,
+            userName: 'PeerAdmin',
+            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
+        };
+        const deployerCard = new IdCard(deployerMetadata, connectionProfile);
+        deployerCard.setCredentials(credentials);
+        const deployerCardName = 'PeerAdmin';
+
+        adminConnection = new AdminConnection({ cardStore: cardStore });
+
+        await adminConnection.importCard(deployerCardName, deployerCard);
+        await adminConnection.connect(deployerCardName);
+    });
+
+    /**
+     *
+     * @param {String} cardName The card name to use for this identity
+     * @param {Object} identity The identity details
+     *//*
+    async function importCardForIdentity(cardName, identity) {
+        const metadata = {
+            userName: identity.userID,
+            version: 1,
+            enrollmentSecret: identity.userSecret,
+            businessNetwork: businessNetworkName
+        };
+        const card = new IdCard(metadata, connectionProfile);
+        await adminConnection.importCard(cardName, card);
+    }
+
+    // This is called before each test is executed.
+    beforeEach(async () => {
+        // Generate a business network definition from the project directory.
+        let businessNetworkDefinition = await BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+        businessNetworkName = businessNetworkDefinition.getName();
+        await adminConnection.install(businessNetworkDefinition);
+        const startOptions = {
+            networkAdmins: [
+                {
+                    userName: 'admin',
+                    enrollmentSecret: 'adminpw'
+                }
+            ]
+        };
+        const adminCards = await adminConnection.start(businessNetworkName, businessNetworkDefinition.getVersion(), startOptions);
+        await adminConnection.importCard(adminCardName, adminCards.get('admin'));
+
+        // Create and establish a business network connection
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+        events = [];
+        businessNetworkConnection.on('event', event => {
+            events.push(event);
+        });
+        await businessNetworkConnection.connect(adminCardName);
+
+        // Get the factory for the business network.
+        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+        const assetRegistry = await businessNetworkConnection.getAssetRegistry(assetNS);
+
+	const assetFER = factory.newResource('org.szg', assetType, 'universityKey:0036');
+	assetFER.opening = 500;
+	assetFER.closing = 2300;
+	assetFER.universityName = "FER";
+	// Create a new relationship for the owner
+	const FERRelation = factory.newRelationship(namespace, assetType, 'universityKey:0036');
+			
+	const assetFSB = factory.newResource(namespace, assetType, 'universityKey:0035');
+	assetFSB.opening = 700;
+	assetFSB.closing = 2100;
+	assetFSB.universityName = "FSB";
+	const FSBRelation = factory.newRelationship(namespace, assetType, 'universityKey:0035');
+
+	const assetFFZG = factory.newResource(namespace, assetType, 'universityKey:0035');
+	assetFFZGopening = 800;
+	assetFFZG.closing = 2000;
+	assetFFZG.universityName = "FFZG";
+	const FFZGRelation = factory.newRelationship(namespace, assetType, 'universityKey:1111');
+
+        assetRegistry.addAll([assetFER, assetFSB, assetFFZG]);
+
+        const participantRegistry = await businessNetworkConnection.getParticipantRegistry(participantNS);
+
+        // Create the participants.
+	var somebody = factory.newResource(namespace, participantType, 'jmbag:0036444444');
+	somebody.firstName = 'marko';
+	somebody.lastName = 'markic';
+	somebody.memberType = 'Student';
+	somebody.universityComponent = FERRelation;
+
+	var somebody2 = factory.newResource(namespace, participantType, 'jmbag:0036555555');
+	somebody2.firstName = 'ana';
+	somebody2.lastName = 'anic';
+	somebody2.memberType = 'Profesor';
+	somebody2.universityComponent = FERRelation;
+
+	var somebody3 = factory.newResource(namespace, participantType, 'jmbag:1111223322');
+	somebody3.firstName = 'filozof';
+	somebody3.lastName = 'filozofic';
+	somebody3.memberType = 'Student';
+	somebody3.universityComponent = FFZGRelation;
+
+        participantRegistry.addAll([somebody, somebody2, somebody3]);
+	
+	const participantRegistry2 = await businessNetworkConnection.getParticipantRegistry(participantNS2);
+	var somebody4 = factory.newResource(namespace, participantType2);
+	var somebody4 = factory.newResource(namespace, participantType2, 'jmbag:1111000000');
+	somebody4.firstName = 'sistemski';
+	somebody4.lastName = 'adminic';
+	somebody4.memberType = 'Staff';
+	somebody4.universityComponent = FFZGRelation;
+	somebody4.jobPosition = "sistemAdmin";
+	await memberRegistry2.add(somebody4);
+
+
+        // Issue the identities.
+        let identity = await businessNetworkConnection.issueIdentity(participantNS + '#0036444444', 'markomarkic');
+        await importCardForIdentity(ferovacCardName, identity);
+        identity = await businessNetworkConnection.issueIdentity(participantNS + '#0036555555', 'anaanic');
+        await importCardForIdentity(ferovacProfesorCardName, identity);
+        identity = await businessNetworkConnection.issueIdentity(participantNS + '#1111223322', 'filozoffilozofic');
+        await importCardForIdentity(filozofCardName, identity);
+    });
+
+    /**
+     * Reconnect using a different identity.
+     * @param {String} cardName The name of the card for the identity to use
+     *//*
+    async function useIdentity(cardName) {
+        await businessNetworkConnection.disconnect();
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+        events = [];
+        businessNetworkConnection.on('event', (event) => {
+            events.push(event);
+        });
+        await businessNetworkConnection.connect(cardName);
+        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+    }
+
+    it('ferovacCardName can read all of the assets', async () => {
+        // Use the identity for ferovacCardName.
+        await useIdentity(ferovacCardName);
+        const assetRegistry = await businessNetworkConnection.getAssetRegistry(assetNS);
+        const assets = await assetRegistry.getAll();
+
+        // Validate the assets.
+        assets.should.have.lengthOf(3);
+        const asset1 = assets[0];
+        asset1.universityName.should.equal('FER');
+        const asset2 = assets[1];
+        asset1.universityName.should.equal('FSB');
+        const asset3 = assets[2];
+        asset1.universityName.should.equal('FFZG');
+    });
+    it('Bob can submit a transaction for his assets', async () => {
+        // Use the identity for ferovacCardName.
+        await useIdentity(ferovacCardName);
+
+        // Submit the transaction.
+        const transaction = factory.newTransaction(namespace, 'AuthorizeAccessFER');
+        transaction.member = factory.newRelationship(namespace, participantType, 'jmbag:0036444444');
+        transaction.universityComponent = factory.newRelationship(namespace, assetType, 'universityKey:0036');
+        await businessNetworkConnection.submitTransaction(transaction);
+
+        // Get the asset.
+        const assetRegistry = await businessNetworkConnection.getAssetRegistry(assetNS);
+        const asset1 = await assetRegistry.get('0036');
+
+        // Validate the asset.
+        asset1.universityName.should.equal('FER');
+	asset1.transactionAuthorized.should.have.lengthOf(1);
+
+        // Validate the events.
+        events.should.have.lengthOf(1);
+        const event = events[0];
+        //event.eventId.should.be.a('string');
+        //event.timestamp.should.be.an.instanceOf(Date);
+        event.memberAccess.universityComponent.universityName.should.equal('FER');
+    });
+});*/
